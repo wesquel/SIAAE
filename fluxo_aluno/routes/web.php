a4a86579e1896915e290cd7ef04e6b956dbc189e
Almir Oliveira <almiroliveira04@gmail.com>
2022-05-15 21:05:34 -0300

    Inicio da criação das rotas para fluxo_aluno

---
+++ fluxo_aluno/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use PHPUnit\TextUI\XmlConfiguration\Group;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::namespace('Site')->group(function(){
+   Route::get('/', 'HomeController');
+ });
