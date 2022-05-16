003d7b2e4de0dc7a0960dd92fc6d883c09cb8a24
Almir Oliveira <almiroliveira04@gmail.com>
2022-05-16 14:51:40 -0300

    Adicionando diretórios

--- fluxo_aluno/resources/views/site/home/index.blade.php
+++ fluxo_aluno/resources/views/site/home/index.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre ExtraBold&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Futura+Bk BT&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}" />
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <style>
@@ -24,42 +24,39 @@
     </style>
 
 </head>
-    <body>
-        <div class="container">
-            <div class=e444_901>
-                <div class="e444_902"></div>
-                <div class="e444_903"></div>
-                <div class="e444_905"></div>
-                <div class="e444_906"></div>
-                <span class="e444_908">Comece agora</span>
-                <div class=e444_909>
-                    <div class="ei444_909_89_62">
-                    </div><span class="ei444_909_89_63">Matrícula</span>
-                    <div class="ei444_909_89_92"></div>
-                    <span class="ei444_909_89_96"></span>
-                </div>
-                <div class=e444_910>
-                    <div class="ei444_910_89_62"></div>
-                    <span class="ei444_910_89_63">Senha</span>
-                    <div class="ei444_910_89_92"></div>
-                    <span class="ei444_910_89_96">**************</span>
-                </div>
-                <div class="e444_911"></div>
-                <div class=e444_931>
-                    <div class="e444_929"></div>
-                    <span class="e444_930">Entrar</span>
-                </div>
-                <div class="e444_914"></div>
-                <div class="e444_915"></div>
-                <div class=e618_2339>
-                    <div class="e618_2340">
-
-                    </div><span class="e618_2341">Comece agora</span>
-                </div>
-            </div>     
-        </div>  
-    </body>
 
 
+<body>
+    <div class=e444_901>
+        <div class="e444_902"></div>
+        <div class="e444_903"></div>
+        <div class="e444_905"></div>
+        <div class="e444_906"></div>
+        <span class="e444_908">Comece agora</span>
+        <div class=e444_909>
+            <div class="ei444_909_89_62">
+            </div><span class="ei444_909_89_63">Matrícula</span>
+            <div class="ei444_909_89_92"></div>
+            <span class="ei444_909_89_96"></span>
+        </div>
+        <div class=e444_910>
+            <div class="ei444_910_89_62"></div>
+            <span class="ei444_910_89_63">Senha</span>
+            <div class="ei444_910_89_92"></div>
+            <span class="ei444_910_89_96">**************</span>
+        </div>
+        <div class="e444_911"></div>
+        <div class=e444_931>
+            <div class="e444_929"></div>
+            <span class="e444_930">Entrar</span>
+        </div>
+        <div class="e444_914"></div>
+        <div class="e444_915"></div>
+        <div class=e618_2339>
+            <div class="e618_2340">
+            </div><span class="e618_2341">Comece agora</span>
+        </div>
+    </div>
+</body>
 
 </html>
