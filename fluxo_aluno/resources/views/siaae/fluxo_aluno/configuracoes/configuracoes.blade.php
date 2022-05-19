633b1f3f3a1c0b08a7df693d16c7710ec2864b8c
Almir Oliveira <almiroliveira04@gmail.com>
2022-05-19 09:26:49 -0300

    Adicionando tela - configurações

--- fluxo_aluno/resources/views/siaae/fluxo_aluno/configuracoes/configuracoes.blade.php
+++ fluxo_aluno/resources/views/siaae/fluxo_aluno/configuracoes/configuracoes.blade.php
@@ -64,84 +64,137 @@
                 </div>
             </div>
         </a>
-        <span class="e357_746">Curriculo:</span><span class="e357_788">Telefone:</span><span class="e448_652">Linkedin:</span><span class="e357_773">Status do Perfil:</span><span class="e448_651">Ativado</span>
+        <span class="e357_746">Curriculo:</span>
+        <span class="e357_788">Telefone:</span>
+        <span class="e448_652">Linkedin:</span>
+        <span class="e357_773">Status do Perfil:</span>
+        <span class="e448_651">Ativado</span>
+        
         <div class=e357_758>
-            <div class=e357_765>
-                <div class="e357_766"></div><span class="e357_767">Enviar Curriculo (PDF,DOC, DOCX e RTF)</span>
+            <div class=e357_765>                
+                <div class="e357_766">
+                    <input type="email" class="e357_766" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Anexe o seu Currículo">
+                </div>
             </div>
         </div>
+
         <div class=e357_778>
-            <div class="ei357_778_89_62"></div><span class="ei357_778_89_63">Telefone</span>
-            <div class="ei357_778_89_92"></div><span class="ei357_778_89_96">Value input</span>
+            <div class="ei357_778_89_62">
+                <input type="email" class="ei357_778_89_62" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Informe seu Telefone">
+            </div>
+            <div class="ei357_778_89_92"></div>
         </div>
+
         <div class=e357_783>
-            <div class="ei357_783_89_62"></div><span class="ei357_783_89_63">Email</span>
-            <div class="ei357_783_89_92"></div><span class="ei357_783_89_96">Value input</span>
+            <div class="ei357_783_89_62">
+                <input type="email" class="ei357_783_89_62" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Informe seu E-mail">
+            </div>
+            <div class="ei357_783_89_92"></div>
         </div>
+
         <div class=e413_524>
-            <div class="ei413_524_89_62"></div><span class="ei413_524_89_63">Linkedin</span>
-            <div class="ei413_524_89_92"></div><span class="ei413_524_89_96">Value input</span>
+            <div class="ei413_524_89_62">
+                <input type="email" class="ei413_524_89_62" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Link para seu Linkedin">
+            </div>
+            <div class="ei413_524_89_92"></div>
         </div>
+
         <div class=e413_529>
-            <div class="ei413_529_89_62"></div><span class="ei413_529_89_63">Lattes</span>
-            <div class="ei413_529_89_92"></div><span class="ei413_529_89_96">Value input</span>
+            <div class="ei413_529_89_62">
+                <input type="email" class="ei413_529_89_62" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Link para seu Lattes">
+            </div>
+            <div class="ei413_529_89_92"></div>
         </div>
+
         <div class=e413_534>
-            <div class="ei413_534_89_62"></div><span class="ei413_534_89_63">Github</span>
-            <div class="ei413_534_89_92"></div><span class="ei413_534_89_96">Value input</span>
-        </div><span class="e357_795">Email:</span><span class="e448_687">Descrição</span><span class="e448_653">Lattes:</span><span class="e448_654">Github:</span><span class="e361_388">CV.pdf</span>
+            <div class="ei413_534_89_62">
+                <input type="email" class="ei413_534_89_62" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Link para seu Github">
+            </div>
+            <div class="ei413_534_89_92"></div>
+            
+        </div>
+
+        <span class="e357_795">Email:</span>
+        <span class="e448_687">Descrição</span>
+        <span class="e448_653">Lattes:</span>
+        <span class="e448_654">Github:</span>
+        <span class="e361_388">CV.pdf</span>
+
         <div class="e448_649"></div>
         <div class="e448_650"></div>
-        <div class=e413_514>
+       <!--  <div class=e413_514>
             <div class="ei413_514_144_319"></div>
             <div class="ei413_514_144_326"></div>
             <div class="ei413_514_144_341"></div><span class="ei413_514_144_320">SOBRE VOCÊ</span>
-        </div>
-        <div class="e448_679"></div><span class="e413_547">Outros contatos</span>
+        </div> -->
+        <span class="e413_547">Outros contatos</span>
+        
         <div class=e448_659>
-            <div class="e448_660"></div><span class="e448_661">TITULO CONTATO</span>
+            <div class="e448_660">
+                <input type="email" class="e448_660" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Título do Contato">
+            </div>
             <div class="e448_662"></div>
-            <div class="e448_663"></div><span class="e448_664">Facebook</span><span class="e448_665">*error mensage</span>
+            <div class="e448_663"></div>
         </div>
+
         <div class=e448_689>
-            <div class="e448_690"></div><span class="e448_691">TITULO CONTATO</span>
+            <div class="e448_690">
+                <input type="email" class="e448_690" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Título do Contato">
+            </div>
             <div class="e448_692"></div>
-            <div class="e448_693"></div><span class="e448_694">Facebook</span><span class="e448_695">*error mensage</span>
+            <div class="e448_693"></div>
         </div>
+
         <div class=e448_666>
-            <div class="e448_667"></div><span class="e448_668">CONTATO</span>
+            <div class="e448_667">
+                <input type="email" class="e448_667" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Contato">
+            </div>
             <div class="e448_669"></div>
-            <div class="e448_670"></div><span class="e448_671">facebook.com/nome/</span><span class="e448_672">*error mensage</span>
+            <div class="e448_670"></div>
         </div>
+
         <div class=e448_696>
-            <div class="e448_697"></div><span class="e448_698">CONTATO</span>
-            <div class="e448_699"></div>
-            <div class="e448_700"></div><span class="e448_701">facebook.com/nome/</span><span class="e448_702">*error mensage</span>
+            <div class="e448_697">
+                <input type="email" class="e448_697" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Contato">
+            </div>
+            <div class="e448_700"></div>
         </div>
+
         <div class=e413_564>
             <div class="ei413_564_45_82"></div><span class="ei413_564_45_83">Remover</span>
         </div>
+
         <div class=e448_688>
             <div class="e448_677"></div><span class="e448_678">+</span>
         </div>
+
         <div class=e413_617>
-            <div class="ei413_617_89_62"></div><span class="ei413_617_89_63">TITULO CONTATO</span>
-            <div class="ei413_617_89_92"></div><span class="ei413_617_89_96">Value input</span>
+            <div class="ei413_617_89_62">
+                <input type="email" class="ei413_617_89_62" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Título do Contato">
+            </div>
+            <div class="ei413_617_89_92"></div>
         </div>
+
         <div class=e413_627>
-            <div class="ei413_627_89_62"></div><span class="ei413_627_89_63">CONTATO</span>
-            <div class="ei413_627_89_92"></div><span class="ei413_627_89_96">Value input</span>
+            <div class="ei413_627_89_62">
+                <input type="email" class="ei413_627_89_62" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Contato">
+            </div>
+            <div class="ei413_627_89_92"></div>
         </div>
-        <div class="e448_703"></div>
+
         <div class=e448_680>
             <div class="ei448_680_444_698"></div>
-            <div class="ei448_680_444_699"></div>
-            <div class="ei448_680_444_700"></div><span class="ei448_680_444_701">Tipo</span>
+            <div class="ei448_680_444_699">
+                <input type="email" class="ei448_680_444_699" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Contato">
+            </div>
+            <div class="ei448_680_444_700"></div>
             <div class="ei448_680_444_702"></div>
         </div>
+        
         <div class=e413_632>
             <div class="ei413_632_45_82"></div><span class="ei413_632_45_83">Adicionar</span>
         </div>
+
         <div class=e618_2299>
             <div class="e618_2300"></div><span class="e618_2301">Atualizar</span>
         </div>
--- fluxo_aluno/resources/views/siaae/fluxo_aluno/home_aluno/home_aluno.blade.php
+++ fluxo_aluno/resources/views/siaae/fluxo_aluno/home_aluno/home_aluno.blade.php
@@ -26,35 +26,58 @@
 <body>
     <div class=e444_934>
         <div class="e444_935"></div>
-        <div class=e618_2288><span class="e618_2289">CONFIGURAÇÕES</span>
-            <div class=e618_2290>
-                <div class="e618_2291"></div>
-            </div>
-        </div>
-        <div class=e618_2262><span class="e618_2263">LISTA DE VAGAS</span>
-            <div class=e618_2264>
-                <div class=ei618_2264_7_1326>
-                    <div class="ei618_2264_7_1327"></div>
-                    <div class="ei618_2264_7_1328"></div>
-                    <div class="ei618_2264_7_1329"></div>
-                </div>
-                <div class=ei618_2264_7_1330>
-                    <div class="ei618_2264_7_1331"></div>
-                    <div class="ei618_2264_7_1332"></div>
-                    <div class="ei618_2264_7_1333"></div>
+        <div class="e444_936"></div>
+        <span class="e444_937">Bem vindo, Fulaninho!</span>
+
+        <a href="/vagas">
+            <div>
+                <div class=e618_2163>
+                    <span class="e618_2164">CADASTRE-SE Á VAGAS</span>
+                    <div class="e618_2165"></div>
                 </div>
             </div>
-        </div>
-        <div class="e444_936"></div><span class="e444_937">Bem vindo, Fulaninho!</span>
-        <div class=e444_948><span class="e444_949">SAIR</span>
-            <div class=e444_950>
-                <div class="e444_951"></div>
+        </a> 
+
+        <a href="/candidaturas">
+            <div>
+                <div class=e618_2262>
+                    <span class="e618_2263">LISTA DE VAGAS</span>
+                    <div class=e618_2264>
+                        <div class=ei618_2264_7_1326>
+                            <div class="ei618_2264_7_1327"></div>
+                            <div class="ei618_2264_7_1328"></div>
+                            <div class="ei618_2264_7_1329"></div>
+                        </div>
+                        <div class=ei618_2264_7_1330>
+                            <div class="ei618_2264_7_1331"></div>
+                            <div class="ei618_2264_7_1332"></div>
+                            <div class="ei618_2264_7_1333"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </a>
+        
+        <a href="/configuracoes">
+            <div>
+                <div class=e618_2288><span class="e618_2289">CONFIGURAÇÕES</span>
+                    <div class=e618_2290>
+                        <div class="e618_2291"></div>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="/login_aluno">
+            <div>
+                <div class=e444_948><span class="e444_949">SAIR</span>
+                <div class=e444_950>
+                    <div class="e444_951"></div>
+                </div>
+                </div>
+            </div>
+        </a>
         <div class="e444_952"></div>
-        <div class=e618_2163><span class="e618_2164">CADASTRE-SE Á VAGAS</span>
-            <div class="e618_2165"></div>
-        </div>
     </div>
 </body>
 
