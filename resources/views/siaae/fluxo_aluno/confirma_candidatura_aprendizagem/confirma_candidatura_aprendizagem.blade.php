ce32abb6b31c2576d30cff4fe0427df83ef259d9
wesqu <wesquel@hotmail.com>
2022-05-22 17:42:19 -0300

    Alteração na pasta da pasta raiz.

---
+++ resources/views/siaae/fluxo_aluno/confirma_candidatura_aprendizagem/confirma_candidatura_aprendizagem.blade.php
@@ -0,0 +1,77 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Html Generated</title>
+    <meta name="description" content="Figma htmlGenerator">
+    <meta name="author" content="htmlGenerator">
+    <link href="https://fonts.googleapis.com/css?family=Futura+Bk BT&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{asset('css/fluxo_aluno/confirma_candidatura_aprendizagem/app.css')}}">
+
+    <style>
+        /*
+                Figma Background for illustrative/preview purposes only.
+                You can remove this style tag with no consequence
+              */
+        body {
+            background: #E5E5E5;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class=e618_2035>
+        <div class=e618_2036>
+            <div class="e618_2037"></div>
+            <div class="e618_2038"></div><span class="e618_2039">Bem vindo, Fulaninho!</span>
+            <div class=e618_2040><span class="e618_2041">CADASTRE-SE Á VAGAS</span>
+                <div class="e618_2042"></div>
+            </div>
+            <div class=e618_2043><span class="e618_2044">LISTA DE VAGAS</span>
+                <div class=e618_2045>
+                    <div class=ei618_2045_7_1326>
+                        <div class="ei618_2045_7_1327"></div>
+                        <div class="ei618_2045_7_1328"></div>
+                        <div class="ei618_2045_7_1329"></div>
+                    </div>
+                    <div class=ei618_2045_7_1330>
+                        <div class="ei618_2045_7_1331"></div>
+                        <div class="ei618_2045_7_1332"></div>
+                        <div class="ei618_2045_7_1333"></div>
+                    </div>
+                </div>
+            </div>
+            <div class=e618_2046><span class="e618_2047">CONFIGURAÇÕES</span>
+                <div class=e618_2048>
+                    <div class="e618_2049"></div>
+                </div>
+            </div>
+            <div class=e618_2050><span class="e618_2051">SAIR</span>
+                <div class=e618_2052>
+                    <div class="e618_2053"></div>
+                </div>
+            </div><span class="e618_2054">Oferta de Vaga de Aprendizagem</span>
+            <div class=e618_2055>
+                <div class="e618_2056"></div>
+                <div class="e618_2057"></div><span class="e618_2058">Voltar</span>
+            </div>
+            <div class="e618_2059"></div><span class="e618_2060">Desenvolvedor Java</span><span class="e618_2061">Pré - Requisitos</span><span class="e618_2062">Atividades Desempenhadas</span><span class="e618_2063">Descritivo da Vaga</span><span class="e618_2064">Empresa X</span><span class="e618_2065">Tipo de Vaga: Aprendizagem</span><span class="e618_2066">Data Limite: 20/02/2022</span><span class="e618_2067">Auxílios: Transporte/Odontologico/Alimentação</span><span class="e618_2068">Possui alguma experiencia com desenvolvimento em Java;
+                Superior Cursando;Conhecimento com Spring seria um diferencial.</span><span class="e618_2069">Realizar integrações de sistemas , desenvolver em java, back end.</span><span class="e618_2070">Estamos procurando um Desenvolvedor Java Júnior para gerenciar o ciclo de vida de desenvolvimento de softwares, desde o planejamento e priorização até as fases de teste e lançamento.</span><span class="e618_2071">Data Início: 20/02/2022</span><span class="e618_2072">Data Fim: 20/02/2022</span><span class="e618_2073">Modalidade: Presencial</span><span class="e618_2074">Bolsa: R$900</span><span class="e618_2075">Turno: Manhã</span><span class="e618_2076">CH semanal: 20h</span>
+            <div class=e618_2077>
+                <div class="ei618_2077_45_82"></div><span class="ei618_2077_45_83">Candidata-se</span>
+            </div>
+        </div>
+        <div class="e618_2078"></div>
+        <div class="e618_2079"></div>
+        <div class=e618_2080><span class="e618_2081">Você candidatou para:
+                Desenvolvedor Java</span>
+            <div class="e618_2082"></div>
+            <div class="e618_2083"></div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/siaae/fluxo_aluno/termos_uso/termos_uso.blade.php
@@ -0,0 +1,174 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Html Generated</title>
+    <meta name="description" content="Figma htmlGenerator">
+    <meta name="author" content="htmlGenerator">
+    <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre ExtraBold&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Futura+Bk BT&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{asset('css/fluxo_aluno/termos_uso/app.css')}}">
+
+    <style>
+        /*
+                Figma Background for illustrative/preview purposes only.
+                You can remove this style tag with no consequence
+              */
+        body {
+            background: #E5E5E5;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class=e561_2333>
+        <div class="e561_2335"></div>
+        <div class="e561_2341"></div>
+        <span class="e561_2346">
+            <h4 class = "h4">Termos e Políticas de Privacidade</h4>
+        </span>
+        <span class = "e561_2347">
+            <h4 class = "h4">FUNDAMENTOS E PRINCÍPIOS DA LEI GERAL DE PROTEÇÃO DE DADOS</h4>
+            O desenvolvimento do tratamento autônomo de proteção dos dados deu lugar a diversas
+            leis que passaram a regulamentar o assunto com o fim de obter um modelo jurídico rico e
+            complexo (DONEDA, 2011, p. 98).
+            A despeito disso, o direito fundamental autônomo a proteção de dados deriva, no Brasil,
+            da constatação dos riscos que o tratamento inadequado dos dados traz à proteção da
+            personalidade da pessoa humana, não decorre de reconhecimento expresso na Constituição
+            Federal da fundamentalidade do direito, como exemplificado no tópico precedente.
+            Hodiernamente, a Lei Geral de Proteção de dados (Lei nº 13. 709/ 2018) regulamenta
+            a proteção de dados em âmbito nacional no país, conciliando a proteção da pessoa, interesse
+            público e incentivo ao desenvolvimento econômico e tecnológico ligados à circulação da
+            informação.
+            Após anos de debates, a LGPD reflete aquilo que vem a ser a espinha dorsal das
+            regulamentações existentes acerca da proteção de dados que tratam das questões principais com
+            as quais o ordenamento irá ter que lidar.
+            Ao longo do artigo 2º, a LGPD expõe seus fundamentos, respeito à privacidade e
+            autodeterminação informativa; liberdade de expressão, informação, comunicação e opinião;
+            inviolabilidade da intimidade, da honra e imagem; desenvolvimento econômico e tecnológico;
+            inovação, livre iniciativa, concorrência e defesa do consumidor; direitos humanos, o livre
+            desenvolvimento da personalidade, dignidade e o exercício da cidadania.
+            Em síntese, ao proteger a inviolabilidade da intimidade, honra, imagem e vida privada,
+            a LGPD vislumbra a salvaguarda da privacidade; ao contemplar o direito ao controle e
+            proteção de dados pessoais, tutela a autodeterminação informativa. Também, preserva a
+            liberdade de expressão, informação, comunicação, livre opinião, a liberdade de iniciativa e livre
+            concorrência, com a defesa do consumidor; tutela a democracia, os direitos mais sensíveis da
+            personalidade da pessoa humana e sua dignidade (SERPRO, 2020).
+            Não obstante, seu maior objetivo encontra-se na garantia de que a pessoa se cientifique
+            acerca de quais dados estão sendo acessados, coletados e armazenados, para qual fim e por
+            quem, o que fica evidente a partir da análise dos princípios que regulamentam a proteção de
+            dados no país dispostos no art. 6º da Lei — boa-fé, finalidade, adequação, necessidade, livre
+            acesso, qualidade dos dados, transparência, segurança, prevenção, não discriminação,
+            responsabilização e prestação de contas.
+            O princípio da boa-fé institui padrão ético de conduta pautado nos ideais de honestidade,
+            lealdade e lisura, de modo a garantir a legitima confiança e expectativa (SILVA; SANTOS,
+            2011, p. 128).
+            Somado a ele, o princípio da finalidade impõe que a utilização dos dados se dê nos
+            exatos moldes que haviam sido elencados no momento do recolhimento, com finalidade
+            legitima, em conformidade com as normas que regulamentam o tratamento de dados do início
+            ao fim (GEDIEL; CORRÊA, 2008, p. 147).
+            O princípio em questão delimita a transmissão dos dados a terceiros e institui critérios
+            para a tratativa de dados, propósitos legítimos, específicos, explícitos e informados ao usuário,
+            impedindo finalidades genéricas e indeterminadas por meio da realização de um juízo de
+            ponderação entre a “utilização de determinados dados para certa finalidade" (DONEDA, 2011,
+            p. 100).
+            Somado a isso, a motivação deverá condizer com a informação solicitada, presentes a
+            pertinência e adequação com relação finalidade informada, conforme traça o princípio da
+            adequação. O tratamento, ainda, deverá se limitar ao mínimo indispensável ao atendimento
+            das finalidades com dados apropriados, não excessivos e proporcionais à finalidade, nos moldes
+            do princípio da necessidade (art. 6º, III, da LGPD).
+            O princípio da proporcionalidade no uso de dados para alcance dos fins os quais se
+            almeja deverá ser, igualmente, atendido.
+            Também, os dados armazenados precisam condizer com a realidade, assim a coleta e
+            tratamento devem ser feitos de modo adequado com cuidado, correção e devida atualização
+            (DONEDA, 2011).
+            Não obstante, o princípio do livre acesso assegura, como o próprio nome já diz, o livre
+            acesso do titular e consulta facilitados à totalidade de seus dados, particularmente, com
+            relação a forma e duração do tratamento. O dono dos dados terá acesso às informações
+            armazenadas, facultada a cópia dos registros e viabilizado o controle dos dados, podendo,
+            inclusive, osretificar, suprimir e complementar com novas informações (BEDENDO;JUNIOR,
+            2010, p. 11).
+            Além disso, o responsável deverá informar o titular dos dados indicando as informações
+            relevantes para o tratamento de dados, até mesmo quanto às especificidades, tais como às
+            relacionadas à forma, fim e tempo de armazenamento dos dados (GEDIEL; CORRÉA, 2008,
+            p. 147), nos exatos termos do trazido pelo princípio da transparência (ou da publicidade). Não
+            só, como também, a LGPD assegura exatidão, clareza, relevância e atualização dos dados, de
+            acordo com a necessidade e para o cumprimento da finalidade deseu tratamento, conforme o
+            princípio da qualidade dos dados impõe.
+            Malgrado, o princípio da segurança física e lógica impõe a necessidade de utilização
+            de medidas técnicas e administrativas eficientes à proteção dos dados. Trata-se da
+            implementação e gerenciamento de um sistema eficiente e abrangente de governança e gestão
+            de riscos de segurança da informação que leve em conta a complexidade da problemática da
+            proteção de dados.
+            O princípio da prevenção impõe, ainda, ao responsável pelos dados, a obrigação de que
+            essas medidas e politicas qualitativas se deem previamente de modo qualitativo com o fim de
+            evitar eventuais danos oriundos do tratamento dos dados.
+            Outrossim, os dados pessoais não poderão ser usados com o fim de promover
+            discriminação do titular dos dados de qualquer natureza, raça, sexo, cor, idade e quaisquer
+            outras formas (art. 3º, III e IV).
+            Caso o uso acarrete prejuízo ou viole quaisquer regras do ordenamento jurídico,ensejará
+            responsabilização civil, nos termos do princípio da responsabilização que exige ainda, o
+            acompanhamento pelas autoridades competentes do processamento dos dados impondo sanções
+            quando houver descumprimento da lei. Paralelamente, o princípio da prestação de contas trata
+            da necessidade de comprovação da adoção das medidas adequadas.
+            Conforme destaca ALMEIDA (et al., 2020, p. 2490), a “conformidade com as leis
+            gerais de proteção de dados, portanto, requer tecnologia, infraestrutura e pessoal especializado
+            para que os dados sejam tratados de forma lícita, justa e responsável em relação aos titulares”.
+            Os dados abrangidos pela LGPD são quaisquer dados que estejam relacionados à pessoa
+            situada no Brasil quando da coleta, independentemente da nacionalidade, do meio aplicado,
+            país-sede do operador ou localidade dos dados. Além disso, abarca todos os setores da
+            economia, com aplicação transversal, multisetorial e extraterritorial.
+            O âmbito de proteção da lei é tão alargado que ao conceituar o que vem a ser dado
+            pessoal a LGPD engloba qualquer informação relacionada à pessoa natural identificada ou
+            identificável, seja dado sensível ou não.
+            A exceção do alcance da lei fica por conta do rol do art. 4 da LGPD, excepcionados os
+            fins exclusivamente particulares e não econômicos e os exclusivamente jornalísticos, artísticos,
+            acadêmicos, bem como os exclusivos de segurança pública, defesa nacional, segurança de
+            Estado ou atividade de investigação e repressão de infrações penais, além dos dados advindos
+            de fora do país e que não sejam objeto de transferência internacional, desde que o país de onde
+            derivarem disponha de proteção adequada.
+            Evidencia-se, assim, que a Lei Geral de Proteção de Dados trata-se de marco para a
+            regulamentação dos dados pessoais no Brasil.
+            Entretanto, apesar de instituir importantes princípios e direitos aos seus titulares a
+            vacatio legis, que em 2018 era de dois anos, foi postergada para agosto de 2020 por meio da
+            Medida Provisória 696/2019, convertida na Lei 13.853/2019.
+            Na sequência, a grave recessão causada pela pandemia do COVID-19 levou a edição do
+            PL 1.179/2020 que estabelece regime jurídico emergencial, ficando decidida, em sede de
+            primeira votação, postergação para janeiro de 2021, a exceção da vacatio das multas esanções
+            que ficariam para agosto de 2021.
+            O imbróglio não parou por aí, a Medida Provisória 959/2020 que passou a vigorar na
+            data de sua publicação, 29 de abril do ano corrente, adiou a vigência da lei para maio de 2021.
+            Até 27 de agosto do ano corrente a Medida deverá ser convertida em lei para que não caduque.
+            Quando ao PL 1.179/2020, o Senado acabou por suprimir o dispositivo que prorrogava
+            a vigência da Lei, ficando mantida a prorrogação apenas no que se refere às sanções.
+            Neste contexto, verifica-se cenário de insegurança com dois resultados possíveis: “sem
+            a MP convertida em lei a LGPD entraria em vigor em 16 de agosto de 2020 (vigência que era
+            prevista antes do início da pandemia) ou, com a conversão em lei da MP, a LGPD entraria em
+            vigor em 3 de maio de 2021” (TUMELERO, 2020).
+            Além disso, até o presente momento o executivo não criou a Autoridade nacional de
+            proteção de Dados (ANPD) que tem a finalidade de fiscalizar a aplicação da LGPD.
+            Especialmente, em situações emergenciais e de interesse público, como no caso da
+            pandemia do COVID-19, os dados se mostram essenciais para a adoção de políticas de
+            enfrentamento, tornando-se a LGPD importante marco regulatório cuja vigência não pode
+            mais ser postergada, dado os prejuízos aos direitos e garantias fundamentais que tutela, isso
+            sem falar nos prejuízos econômicos.
+        </span>
+        <div class=e561_2343>
+            <div class="e561_2344"></div>
+            <span class="e561_2345">
+                <input type="checkbox" id="horns" name="horns" class = "checkbox_t">
+                Declaro que li e aceito os termos e politica de privacidade
+            </span>
+        </div>
+        <a href="/home_aluno">
+            <div class=button>
+                <div class = "texto_button">Continuar</div>
+             </div>
+        </a>
+    </div>
+</body>
+
+</html>
