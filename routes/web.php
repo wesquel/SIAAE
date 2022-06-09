166a8091d21c10781aac7b629524ec91bbcd10ac
wesqu <wesquel@hotmail.com>
2022-06-09 11:55:39 -0300

    Remoção de linha inutil.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use PHPUnit\TextUI\XmlConfiguration\Group;
 use App\Http\Controllers\siaae\ControllerAluno;
-use App\Http\Controllers\testeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
