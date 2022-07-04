62c745e5db00fbe700ad83da9698df453ed7d5ca
wesqu <wesquel@hotmail.com>
2022-07-04 08:44:10 -0300

    Adicionado pagina de error 404, junto as views padrão de error do laravel na pasta resouce.

---
+++ resources/views/error_views/error_404.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset='utf-8'>
+        <title>SIAAE - Sistema Integrado de Apoio a Aprendizagem e Estágios</title>
+        <link rel="icon" type="image/x-icon" href="{{asset('/images/favicon-siaae.ico')}}">
+        <link rel="stylesheet" type='text/css' media='screen' href="{{asset('css/error.css')}}">
+    </head>
+    <body>
+    <div class="div-img">
+        <img class="img-error"  src="{{asset('/images/404_not_found_1.png')}}">
+        <p class="text-error">NOT FOUND</p>
+    </div>
+    </body>
+</html>
