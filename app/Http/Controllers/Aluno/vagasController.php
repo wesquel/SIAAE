896950de2d2fcfaae4e8644111ac87f8ba7a6619
wesqu <wesquel@hotmail.com>
2022-07-24 15:59:54 -0300

    Adicionado vagas cadastradas do aluno na lista de vagas do aluno.

--- app/Http/Controllers/Aluno/vagasController.php
+++ app/Http/Controllers/Aluno/vagasController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Aluno;
 
 use App\Http\Controllers\Controller;
 use App\Models\Aluno;
+use App\Models\Vaga;
+use App\Models\VagasAlunosEmpresa;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,8 +13,17 @@ use Illuminate\Support\Facades\Auth;
 class vagasController extends Controller
 {
     public function create(){
+        $vagas = $this->getVagas();
+        return view('aluno.vagas', ['vagas' => $vagas]);
+    }
 
-        return view('aluno.vagas');
+    public function getVagas(){
+        $vagas_id = VagasAlunosEmpresa::where('aluno_id', '=', Auth::user()->id)->get();
+        $vagas = [];
+        foreach ($vagas_id as $vaga){
+            $vagas[] = [Vaga::where('id', '=', $vaga['vaga_id'])->get(), $vaga['status_aluno']];
+        }
+        return $vagas;
     }
 
     public function createEstagio(){
--- resources/views/aluno/vagas.blade.php
+++ resources/views/aluno/vagas.blade.php
@@ -20,21 +20,29 @@
         <table class="row">
             <thead>
                 <tr class="row">
-                    <td class="col-md-3">Nome</td>
-                    <td class="col-md-3">Data de publicação</td>
                     <td class="col-md-3">Titulo</td>
+                    <td class="col-md-3">Data de publicação</td>
+                    <td class="col-md-3">Tipo</td>
                     <td class="col-md-3" style="text-align: right">Status</td>
                 </tr>
             </thead>
             <tbody>
-                @for($i = 0; $i < 5; $i++)
+                @foreach($vagas as $vaga)
                     <tr class="row">
-                        <td class="col-md-3">Desenvolvedor Java</td>
+                        <td class="col-md-3">{{$vaga[0][0]['titulo']}}</td>
                         <td class="col-md-3">12/04/2021</td>
-                        <td class="col-md-3">Estágio</td>
-                        <td class="col-md-3" style="text-align: right">Aceito <label class="system-icon-circle aceito" ></label></td>
+                        <td class="col-md-3">{{$vaga[0][0]['tipo']}}</td>
+                        @if($vaga[1] == 2)
+                            <td class="col-md-3" style="text-align: right">Aceito <label class="system-icon-circle aceito" ></label></td>
+                        @endif
+                        @if($vaga[1] == 1)
+                            <td class="col-md-3" style="text-align: right">Em análise <label class="system-icon-circle em-analise" ></label></td>
+                        @endif
+                        @if($vaga[1] == 0)
+                            <td class="col-md-3" style="text-align: right">Recusado <label class="system-icon-circle recusado" ></label></td>
+                        @endif
                     </tr>
-                @endfor
+                @endforeach
             </tbody>
         </table>
     </div>
