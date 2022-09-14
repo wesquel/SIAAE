3444413c1f448c6c05d925a754b4dacf0949cfb6
wesqu <wesquel@hotmail.com>
2022-09-14 20:05:23 -0300

    Input de entrada no cadastro de vagas atualizado para o padrão siaae.

--- resources/views/empresa/cadastrar_vaga.blade.php
+++ resources/views/empresa/cadastrar_vaga.blade.php
@@ -2,6 +2,7 @@
 
 @push('custom-styles')
     <link rel="stylesheet" href="{{ asset('css/empresa/cadastrarvaga.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/empresa/cadastroDeVagas.css') }}">
 @endpush
 
 @section('content')
@@ -23,56 +24,56 @@
                     <div class="input-contain">
                         <input class="input-new" name="cadastro" type="text" value="{{Auth::user()->nome_empresa}}" disabled>
                         <label class="placeholder-text" for="cadastro">
-                            <div class="text">Nome empresa:</div>
+                            <div class="label-new">Nome empresa:</div>
                         </label>
                     </div>
                     <div class="input-contain linha-vertical">
                         <input class="input-new" name="cadastro" type="text" value="{{Auth::user()->CPF_CNPJ}}" disabled>
                         <label class="placeholder-text" for="cadastro">
-                            <div class="text">CPF/CNPJ:</div>
+                            <div class="label-new">CPF/CNPJ:</div>
                         </label>
                     </div>
                     <div class="input-contain linha-vertical">
                         <input class="input-new" name="cadastro" type="text" value="{{Auth::user()->telefone}}" disabled>
                         <label class="placeholder-text" for="cadastro">
-                            <div class="text">Telefone:</div>
+                            <div class="label-new">Telefone:</div>
                         </label>
                     </div>
                     <div class="input-contain linha-vertical">
                         <input class="input-new" name="cadastro" type="text" value="{{Auth::user()->email}}" disabled>
                         <label class="placeholder-text" for="cadastro">
-                            <div class="text">Email:</div>
+                            <div class="label-new">Email:</div>
                         </label>
                     </div>
                     <div class="input-contain linha-vertical">
                         <input class="input-new" name="cadastro" type="text" value="{{$endereco[0]->cep}}" disabled>
                         <label class="placeholder-text" for="cadastro">
-                            <div class="text">CEP:</div>
+                            <div class="label-new">CEP:</div>
                         </label>
                     </div>
                     <div class="input-contain linha-vertical">
                         <input class="input-new" name="cadastro" type="text" value="{{$endereco[0]->estado}}" disabled>
                         <label class="placeholder-text" for="cadastro">
-                            <div class="text">Estado:</div>
+                            <div class="label-new">Estado:</div>
                         </label>
                     </div>
                     <div class="input-contain linha-vertical">
                         <input class="input-new" name="cadastro" type="text" value="{{$endereco[0]->cidade}}" disabled>
                         <label class="placeholder-text" for="cadastro">
-                            <div class="text">Cidade:</div>
+                            <div class="label-new">Cidade:</div>
                         </label>
                     </div>
                     <div class="div-bairro-num">
                         <div class="input-contain bairro-input">
                             <input class="input-new" name="cadastro" type="text" value="{{$endereco[0]->bairro}}" disabled>
                             <label class="placeholder-text" for="cadastro">
-                                <div class="text">Bairro:</div>
+                                <div class="label-new">Bairro:</div>
                             </label>
                         </div>
                         <div class="input-contain linha-vertical numero-input">
                             <input class="input-new" name="cadastro" type="text" value="{{$endereco[0]->numero}}" disabled>
                             <label class="placeholder-text" for="cadastro">
-                                <div class="text">Número:</div>
+                                <div class="label-new">Número:</div>
                             </label>
                         </div>
                     </div>
@@ -80,7 +81,7 @@
                     <div class="input-contain linha-vertical">
                         <input class="input-new" name="cadastro" type="text" value="{{$endereco[0]->logradouro}}" disabled>
                         <label class="placeholder-text" for="cadastro">
-                            <div class="text">Logradouro:</div>
+                            <div class="label-new">Logradouro:</div>
                         </label>
                     </div>
                     @endif
